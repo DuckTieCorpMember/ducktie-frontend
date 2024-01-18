bd7d9cc2d37f1844d5bdf467a16287553233fc75
AssylbekDanyshbek <danyshbek1996@gmail.com>
2024-01-18 15:08:42 +0600

    Wokring on OTP

--- src/otp-component/OTPComponent.tsx
+++ src/otp-component/OTPComponent.tsx
@@ -1,16 +1,31 @@
-import React, { useCallback, useState } from 'react';
+import React, { ChangeEvent, useCallback, useState } from 'react';
 import "./OTPComponent.css";
 
 function OTPComponent(props: any) {
     const otpCode = props.otpCode;
-    const [inputCode, setInputCode] = useState([0,0,0,0]);
+    const [inputCode, setInputCode] = useState(["0","0","0","0"]);
 
     const checkInput = useCallback((input: string) => {
-        return input === otpCode
+      return input === otpCode
     }, [])
 
     const inputKeyDown = useCallback((event: any, id: number) => {
-        console.log(event);
+      console.log("KeyDown", event);
+      let newInput = [...inputCode];
+      if(/^[0-9]$/i.test(event.key)){
+        newInput[id] = event.key;
+
+        setInputCode(newInput);
+      }
+
+      if(event.code === "Backspace" || event.code === "Delete"){
+        newInput[id] = "";
+        setInputCode(newInput);
+      }
+    }, []);
+
+    const onChange = useCallback((event: ChangeEvent, id: number) => {
+      // console.log("change", event, id);
     }, []);
 
     return (
@@ -18,10 +33,10 @@ function OTPComponent(props: any) {
         <div className="otp-component-title">Enter the code ...</div>
         <div className="otp-component-input-wrapper">
             <div className="otp-component-input-wrapper">
-                <input className='otp-component-input-input' maxLength={1} onKeyDown={(event) => inputKeyDown(event, 0)}></input>
-                <input className='otp-component-input-input' maxLength={1} onKeyDown={(event) => inputKeyDown(event, 1)}></input>
-                <input className='otp-component-input-input' maxLength={1} onKeyDown={(event) => inputKeyDown(event, 2)}></input>
-                <input className='otp-component-input-input' maxLength={1} onKeyDown={(event) => inputKeyDown(event, 3)}></input>
+                <input className='otp-component-input-input' onChange={(event) => onChange(event, 0)} value={inputCode[0]} maxLength={1} onKeyDown={(event) => inputKeyDown(event, 0)}></input>
+                <input className='otp-component-input-input' onChange={(event) => onChange(event, 1)} value={inputCode[1]} maxLength={1} onKeyDown={(event) => inputKeyDown(event, 1)}></input>
+                <input className='otp-component-input-input' onChange={(event) => onChange(event, 2)} value={inputCode[2]} maxLength={1} onKeyDown={(event) => inputKeyDown(event, 2)}></input>
+                <input className='otp-component-input-input' onChange={(event) => onChange(event, 3)} value={inputCode[3]} maxLength={1} onKeyDown={(event) => inputKeyDown(event, 3)}></input>
             </div>
         </div>
         <button className='otp-component-submit-button'>SUBMIT</button>
