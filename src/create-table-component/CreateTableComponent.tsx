2a26c6ff4e5742cee1eb1c445223660e9edc73ac
AssylbekDanyshbek <danyshbek1996@gmail.com>
2024-02-06 15:40:43 +0600

    still creating table

--- src/create-table-component/CreateTableComponent.tsx
+++ src/create-table-component/CreateTableComponent.tsx
@@ -17,30 +17,38 @@ function CreateTableComponent() {
         (event.target as HTMLElement).hidden = true;
 
         setDataSet([{
-            "Header 1": ""   
+            Header1: "Row 1 H1",
+            Header2: "Row 1 H2"   
+        },{
+            Header1: "Row 2 H1",
+            Header2: "Row 2 H2"
         }]);
-
-        // generateStoredTable();
     }, [])
-
-    const generateNewRow = () => {
-        return (
-            <button className="button button-success add-table-button" onClick={generateNewTable}>+</button>
-        )
-    }
-
-    const generateStoredTable = () => {
-        // console.log("Table generator");
-        return (<div>Stored</div>)
-    }
 
     return (
       <div>
         <div className="table-creator-container" style={{height: height, width: width}}>
             {dataSet.length === 0 ? (
-                generateNewRow()
+                <button className="button button-success add-table-button" onClick={generateNewTable}>+</button>
             ) : (
-                generateStoredTable()
+                <div className="table-header-row">
+                    {
+                        Object.keys(dataSet[0]).map((key, index) => {
+                            return <div className="table-header-cell-container" key={index}>
+                                <div className="table-header-cell-item">{key}</div>
+                            </div>
+                        })
+                    }
+                </div>
+                //create header row for each column
+                    //decide what is active editor or cell
+            
+                //create rows
+                    //decide what is actice edior or cell
+                
+                //render button on the left
+
+                //render button on the right
             )}
         </div>
       </div>
