49a90f45c336c9a861ef1f020af717d1945815f7
AssylbekDanyshbek <danyshbek1996@gmail.com>
2024-02-09 10:16:17 +0600

    creating table

--- src/create-table-component/CreateTableComponent.tsx
+++ src/create-table-component/CreateTableComponent.tsx
@@ -1,11 +1,6 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import "./CreateTableComponent.css";
-
-const columnTypes = [
-    "Number",
-    "String"
-]
 
 function CreateTableComponent() {
     const [height, setHeight] = useState(100);
@@ -15,7 +10,6 @@ function CreateTableComponent() {
 
     const generateNewTable = useCallback((event: React.MouseEvent<HTMLElement>) => {
         (event.target as HTMLElement).hidden = true;
-
         setDataSet([{
             Header1: "Row 1 H1",
             Header2: "Row 1 H2"   
@@ -25,30 +19,53 @@ function CreateTableComponent() {
         }]);
     }, [])
 
+    useEffect(() => {
+        if(dataSet.length>0){
+            setWidth(Object.keys(dataSet[0]).length*200+50);
+            setHeight((dataSet.length+1)*50+50);
+        }
+    }, [dataSet]);
+
     return (
       <div>
         <div className="table-creator-container" style={{height: height, width: width}}>
             {dataSet.length === 0 ? (
                 <button className="button button-success add-table-button" onClick={generateNewTable}>+</button>
             ) : (
+                <div className="table-creator-main-wrapper">
+                {/* // DONE: create header row for each column                     */}
                 <div className="table-header-row">
                     {
                         Object.keys(dataSet[0]).map((key, index) => {
+                            // decide what is active editor or cell
                             return <div className="table-header-cell-container" key={index}>
                                 <div className="table-header-cell-item">{key}</div>
                             </div>
                         })
                     }
                 </div>
-                //create header row for each column
-                    //decide what is active editor or cell
             
-                //create rows
-                    //decide what is actice edior or cell
-                
-                //render button on the left
+                {/* //create rows */}
+                    {/* //decide what is actice edior or cell */}
+                <div className="table-creator-rows-container">
+                    {
+                        dataSet.map((data, index) => {
+                            return <div key={index} className="table-creator-row">
+                                {
+                                    Object.keys(dataSet[index]).map((key, jIndex) => {
+                                        return <div className="table-creator-cell" key={jIndex}>
+                                            <div>{data[key]}</div>
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        })
+                    }
+                </div>
+                {/* //render button on the left */}
 
-                //render button on the right
+                {/* //render button on the right */}
+                </div>
             )}
         </div>
       </div>
